0b10465dacc723c44a676acc3c7b3095a44d5765
zhiqian.liu <zhiqian.liu@baifendian.com>
2021-06-05 17:15:36 +0800

    [develop] util-develop

--- packages/adherev-website/src/introduction.vue
+++ packages/adherev-website/src/introduction.vue
@@ -1,17 +1,75 @@
 <template>
-  <div>
-    introduction
+  <div :class="$style.Wrap">
+    <article class="ant-typography">
+      <h2 class="ant-typography">概述</h2>
+      <div class="ant-typography">
+        <span class="ant-typography">
+          这个工程中包含很多个 npm 包，这些包都是在平时工作中沉淀出来的一些可以高度复用的组件，有 UI
+          相关、功能相关、GIS 相关,使用的是 Vue 技术，有的可能是对
+          ant-design-vue(还有其他第三方的库)的二次封装
+        </span>
+      </div>
+      <h2 class="ant-typography">兼容</h2>
+      <div class="ant-typography">
+        <ul>
+          <li>- 支持vue2.x</li>
+          <li>- 支持ant-design-vue</li>
+          <li>- 支持国际化</li>
+          <li>- 支持修改主题</li>
+          <li>- 支持动态引入(babel-plugin-import)</li>
+        </ul>
+      </div>
+      <h2 class="ant-typography">安装</h2>
+      <div class="ant-typography"><code>npm i @baifendian/adherev --save</code></div>
+      <h2 class="ant-typography">动态引入</h2>
+    </article>
+
+    <article class="ant-typography" style="margin-bottom: 20px">
+      <div class="ant-typography">
+        需要在webpack构建中加入如下配置,如果进行了动态引入，则不就需要要单独引入
+        <code>@baifendian/adherev/lib/index.less</code>和<code>ant-design-vue/dist/antd.less</code>
+        <br />
+        如果没有进行动态引入则需要单独引入<code>@baifendian/adherev/lib/index.less</code>和
+        <code>ant-design-vue/dist/antd.less</code>
+      </div>
+    </article>
+
+    <playground :code-text="code1" :default-expand="true" lang="javascript" />
   </div>
 </template>
 
 <script>
-
+import Playground from '@/lib/Playground/Playground';
 export default {
-
-}
-
+  components: { Playground },
+  data() {
+    return {
+      code1: `
+        [
+          'import',
+          {
+            libraryName: '@baifendian/adherev',
+            transformToDefaultImport: true,
+            style: true,
+          },
+          'adherev',
+        ],
+        [
+          'import',
+          {
+            libraryName: 'ant-design-vue',
+            style: true,
+          },
+          'ant-design-vue',
+        ],
+      `,
+    };
+  },
+};
 </script>
 
 <style lang="less" module>
-
+.Wrap {
+  padding: 20px;
+}
 </style>
--- packages/adherev/src/index.ts
+++ packages/adherev/src/index.ts
@@ -26,7 +26,7 @@ import Preferences from '@baifendian/adherev-util-preferences';
 import Resource from '@baifendian/adherev-util-resource';
 import WatchMemoized from '@baifendian/adherev-util-watchmemoized';
 
-export {
+export { 
   ConditionalRender,
   DelConfirm,
   ImportantConfirm,
@@ -57,195 +57,141 @@ export {
 };
 
 export default {
-  install: function (Vue) {
-    if (
-      // @ts-ignore
-      ConditionalRender.isUse()
-    ) {
-      // @ts-ignore
-      ConditionalRender.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      DelConfirm.isUse()
-    ) {
-      // @ts-ignore
-      DelConfirm.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      ImportantConfirm.isUse()
-    ) {
-      // @ts-ignore
-      ImportantConfirm.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      GlobalIndicator.isUse()
-    ) {
-      // @ts-ignore
-      GlobalIndicator.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      HistoryBack.isUse()
-    ) {
-      // @ts-ignore
-      HistoryBack.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      MessageDialog.isUse()
-    ) {
-      // @ts-ignore
-      MessageDialog.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      Permission.isUse()
-    ) {
-      // @ts-ignore
-      Permission.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      ErrorPrompt.isUse()
-    ) {
-      // @ts-ignore
-      ErrorPrompt.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      SuccessPrompt.isUse()
-    ) {
-      // @ts-ignore
-      SuccessPrompt.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      WarnPrompt.isUse()
-    ) {
-      // @ts-ignore
-      WarnPrompt.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      Space.isUse()
-    ) {
-      // @ts-ignore
-      Space.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      Spin.isUse()
-    ) {
-      // @ts-ignore
-      Spin.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      Split.isUse()
-    ) {
-      // @ts-ignore
-      Split.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      Suspense.isUse()
-    ) {
-      // @ts-ignore
-      Suspense.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      FlexLayout.isUse()
-    ) {
-      // @ts-ignore
-      FlexLayout.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      SearchTable.isUse()
-    ) {
-      // @ts-ignore
-      SearchTable.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      OLMap.isUse()
-    ) {
-      // @ts-ignore
-      OLMap.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      Util.isUse()
-    ) {
-      // @ts-ignore
-      Util.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      AdapterScreen.isUse()
-    ) {
-      // @ts-ignore
-      AdapterScreen.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      Ajax.isUse()
-    ) {
-      // @ts-ignore
-      Ajax.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      Dict.isUse()
-    ) {
-      // @ts-ignore
-      Dict.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      Emitter.isUse()
-    ) {
-      // @ts-ignore
-      Emitter.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      Intl.isUse()
-    ) {
-      // @ts-ignore
-      Intl.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      NotNull.isUse()
-    ) {
-      // @ts-ignore
-      NotNull.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      Preferences.isUse()
-    ) {
-      // @ts-ignore
-      Preferences.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      Resource.isUse()
-    ) {
-      // @ts-ignore
-      Resource.use(Vue);
-    }
-    if (
-      // @ts-ignore
-      WatchMemoized.isUse()
-    ) {
-      // @ts-ignore
-      WatchMemoized.use(Vue);
-    }
-  },
+	install:function (Vue){
+	if(
+// @ts-ignore
+ConditionalRender.isUse()){ 
+// @ts-ignore
+ConditionalRender.use(Vue); }
+		if(
+// @ts-ignore
+DelConfirm.isUse()){ 
+// @ts-ignore
+DelConfirm.use(Vue); }
+		if(
+// @ts-ignore
+ImportantConfirm.isUse()){ 
+// @ts-ignore
+ImportantConfirm.use(Vue); }
+		if(
+// @ts-ignore
+GlobalIndicator.isUse()){ 
+// @ts-ignore
+GlobalIndicator.use(Vue); }
+		if(
+// @ts-ignore
+HistoryBack.isUse()){ 
+// @ts-ignore
+HistoryBack.use(Vue); }
+		if(
+// @ts-ignore
+MessageDialog.isUse()){ 
+// @ts-ignore
+MessageDialog.use(Vue); }
+		if(
+// @ts-ignore
+Permission.isUse()){ 
+// @ts-ignore
+Permission.use(Vue); }
+		if(
+// @ts-ignore
+ErrorPrompt.isUse()){ 
+// @ts-ignore
+ErrorPrompt.use(Vue); }
+		if(
+// @ts-ignore
+SuccessPrompt.isUse()){ 
+// @ts-ignore
+SuccessPrompt.use(Vue); }
+		if(
+// @ts-ignore
+WarnPrompt.isUse()){ 
+// @ts-ignore
+WarnPrompt.use(Vue); }
+		if(
+// @ts-ignore
+Space.isUse()){ 
+// @ts-ignore
+Space.use(Vue); }
+		if(
+// @ts-ignore
+Spin.isUse()){ 
+// @ts-ignore
+Spin.use(Vue); }
+		if(
+// @ts-ignore
+Split.isUse()){ 
+// @ts-ignore
+Split.use(Vue); }
+		if(
+// @ts-ignore
+Suspense.isUse()){ 
+// @ts-ignore
+Suspense.use(Vue); }
+		if(
+// @ts-ignore
+FlexLayout.isUse()){ 
+// @ts-ignore
+FlexLayout.use(Vue); }
+		if(
+// @ts-ignore
+SearchTable.isUse()){ 
+// @ts-ignore
+SearchTable.use(Vue); }
+		if(
+// @ts-ignore
+OLMap.isUse()){ 
+// @ts-ignore
+OLMap.use(Vue); }
+		if(
+// @ts-ignore
+Util.isUse()){ 
+// @ts-ignore
+Util.use(Vue); }
+		if(
+// @ts-ignore
+AdapterScreen.isUse()){ 
+// @ts-ignore
+AdapterScreen.use(Vue); }
+		if(
+// @ts-ignore
+Ajax.isUse()){ 
+// @ts-ignore
+Ajax.use(Vue); }
+		if(
+// @ts-ignore
+Dict.isUse()){ 
+// @ts-ignore
+Dict.use(Vue); }
+		if(
+// @ts-ignore
+Emitter.isUse()){ 
+// @ts-ignore
+Emitter.use(Vue); }
+		if(
+// @ts-ignore
+Intl.isUse()){ 
+// @ts-ignore
+Intl.use(Vue); }
+		if(
+// @ts-ignore
+NotNull.isUse()){ 
+// @ts-ignore
+NotNull.use(Vue); }
+		if(
+// @ts-ignore
+Preferences.isUse()){ 
+// @ts-ignore
+Preferences.use(Vue); }
+		if(
+// @ts-ignore
+Resource.isUse()){ 
+// @ts-ignore
+Resource.use(Vue); }
+		if(
+// @ts-ignore
+WatchMemoized.isUse()){ 
+// @ts-ignore
+WatchMemoized.use(Vue); }
+		},
 };
