49449da87ca947de8252b9c96466c82a8d290b81
zhiqian.liu <1qaz@WSX>
2022-10-18 17:11:57 +0800

    feat: 同步adherev1.0修改

---
+++ packages/adherev-ui-writingboard/es/index.js
@@ -0,0 +1,2 @@
+import Util from"@baifendian/adherev-util";import WritingBoard from"./writingboard";var _a=Util._util,withInstall=_a.withInstall,withVue=_a.withVue,Component=withInstall(WritingBoard);Component.isUse=function(){return!0},Component.use=function(t){t.use(Component),withVue(t,"WritingBoard",Component)};export default Component;
+//# sourceMappingURL=index.js.map
---
+++ packages/adherev-ui-writingboard/es/writingboard.d.ts
@@ -0,0 +1,33 @@
+declare const _default: import("vue").DefineComponent<{
+    defaultMode: import("vue-types").VueTypeValidableDef<string> & {
+        default: string;
+    };
+    defaultStrokeStyle: import("vue-types").VueTypeValidableDef<string> & {
+        default: string;
+    };
+    defaultLineWidth: import("vue-types").VueTypeValidableDef<number> & {
+        default: number;
+    };
+    resizeTime: import("vue-types").VueTypeValidableDef<number> & {
+        default: number;
+    };
+}, () => JSX.Element, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    defaultMode: import("vue-types").VueTypeValidableDef<string> & {
+        default: string;
+    };
+    defaultStrokeStyle: import("vue-types").VueTypeValidableDef<string> & {
+        default: string;
+    };
+    defaultLineWidth: import("vue-types").VueTypeValidableDef<number> & {
+        default: number;
+    };
+    resizeTime: import("vue-types").VueTypeValidableDef<number> & {
+        default: number;
+    };
+}>>, {
+    defaultMode: string;
+    defaultStrokeStyle: string;
+    defaultLineWidth: number;
+    resizeTime: number;
+}>;
+export default _default;
