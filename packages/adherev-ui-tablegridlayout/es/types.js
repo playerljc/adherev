4c53b96c923cf88df47bf29d74f663507394ac5e
zhiqian.liu <1qaz@WSX>
2022-10-21 14:44:35 +0800

    feat: tablegridlayout添加

---
+++ packages/adherev-ui-tablegridlayout/es/types.js
@@ -0,0 +1,2 @@
+import{array,bool,object,string}from"vue-types";var tableGridLayoutProps={className:string().def(""),style:object().def({}),innerClassName:string().def(""),innerStyle:object().def({}),bordered:bool().def(!1),data:array().def([]),layout:string().def("horizontal"),density:string().def("default"),parity:bool().def(!1)};export{tableGridLayoutProps};
+//# sourceMappingURL=types.js.map
--- packages/adherev-ui-tablegridlayout/src/demo/index.ts
+++ packages/adherev-ui-tablegridlayout/src/demo/index.ts
@@ -1,27 +1,27 @@
-import { Button, Card, DatePicker, Form, Input, Radio } from 'ant-design-vue';
-import { createApp, h } from 'vue';
-
-import Space from '@baifendian/adherev-ui-space';
-
-import TableGridLayout from '../index';
-// @ts-ignore
-import App from './index.vue';
-
-import '../index.less';
-import './index.less';
-
-const app = createApp({
-  render() {
-    return h(App);
-  },
-});
-
-Space.use(app);
-TableGridLayout.use(app);
-app.use(Card);
-app.use(Button);
-app.use(Form);
-app.use(DatePicker);
-app.use(Input);
-app.use(Radio);
-app.mount('#app');
+// import { Button, Card, DatePicker, Form, Input, Radio } from 'ant-design-vue';
+// import { createApp, h } from 'vue';
+//
+// import Space from '@baifendian/adherev-ui-space';
+//
+// import TableGridLayout from '../index';
+// // @ts-ignore
+// import App from './index.vue';
+//
+// import '../index.less';
+// import './index.less';
+//
+// const app = createApp({
+//   render() {
+//     return h(App);
+//   },
+// });
+//
+// Space.use(app);
+// TableGridLayout.use(app);
+// app.use(Card);
+// app.use(Button);
+// app.use(Form);
+// app.use(DatePicker);
+// app.use(Input);
+// app.use(Radio);
+// app.mount('#app');
